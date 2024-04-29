545b49cd3cdb7db6f883b5edcdaf358ffa52f339
aaraf.younus <aarafrao22@gmail.com>
2024-04-29 12:12:21 +0500

    TODO

--- app/src/main/java/com/example/mikelesimonieventfindercs414hw4/MainActivity.kt
+++ app/src/main/java/com/example/mikelesimonieventfindercs414hw4/MainActivity.kt
@@ -31,6 +31,9 @@ class MainActivity : AppCompatActivity(), ShakeListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        TODO("Integrate Add to Fav Feature as well")
+
         // set components
         searchButton = findViewById(R.id.searchbutton)
         eventTypeInput = findViewById(R.id.keywordInput)
